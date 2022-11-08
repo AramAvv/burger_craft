6703e8a20aa5f8252f96c0f721ffb986bf74e5a3
Corina Morari <morarik8@gmail.com>
2022-11-08 09:32:48 +0200

    Version 1.0

--- script/script.js
+++ script/script.js
@@ -1,43 +1,94 @@
-const fetch1 = fetch("./script/package.json")
-  .then((response) => response.json())
-  .then((data) => Object.entries(data));
+const fetch1 = fetch("http://localhost:3000/ingridients")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    });
 console.log(fetch1);
+// fetch(http://localhost:3000/order', {
+//   method: 'POST',
+//   headers: {
+//     'Accept': 'application/json, text/plain, */*',
+//     'Content-Type': 'application/json'
+//   },
+//   body: JSON.stringify({a: 7, str: 'Some string: &=&'})
+// }).then(res => res.json())
+//     .then(res => console.log(res));
 
-const body = document.querySelector("body");
-const add_item = document.querySelector("button");
+let images = document.querySelectorAll('.burger_image')
+const add_meat_btn = document.querySelector('.btn_meat')
 const meat_div = document.querySelector(".burger_item_meat");
+const select_meat = document.querySelectorAll('.burger_item_meat option')
+const meat_box = document.querySelector('.meat_box')
+let select_meat_arr = document.querySelectorAll('.burger_item_meat select');
 
-add_item.addEventListener("click", () => {
+window.addEventListener('load',()=>{
+    add_arrow(images);
+})
+
+add_meat_btn.addEventListener("click", () => {
+
   const select = document.createElement("select");
   meat_div.append(select);
-  let items = [
-    "BEEF MEET(35 lei)",
-    "BREADED CHICKEN FILLET(25 lei)",
-    "GRILLED CHICKEN FILLET(25 lei)",
-  ];
-  const meat_image = document.querySelector(".burger_meat_image");
-  const newMeat_image = document.createElement("img");
-  newMeat_image.src = "img/pirjoala-de-vita.svg";
-  meat_image.append(newMeat_image);
-  for (let i = 0; i < items.length; i++) {
-    let item = items[i];
-    const newOption = document.createElement("option");
-    newOption.setAttribute("value", item);
-    const textNode = document.createTextNode(item);
-    newOption.appendChild(textNode);
-    select.append(newOption);
-  }
+  select.setAttribute('name','meat')
+  select_meat.forEach(item=>{
+    console.log(item)
+    const option = document.createElement('option')
+    option.setAttribute('value',item.getAttribute('value'));
+    option.textContent = item.textContent;
+    select.append(option)
+  })
+  add_img(meat_box,'../img/pirjoala-de-vita.svg')
+  select_meat_arr = document.querySelectorAll('.burger_item_meat select');
+  console.log(select_meat_arr);
+
 });
 
-// const burger_img_detailes = document.querySelectorAll('.burger_image')
-// const add_arrow = (arr) => {
-//   arr.forEach((item, index) =>{
-//     console.log(item)
-//     if(index % 2 === 0){
-//       item.classList.toggle('burger_image_before')
-//     }else {
-//       item.classList.toggle('burger_image_after')
-//     }
+
+// const changes = () => {
+//   select_meat_arr.forEach(item =>{
+//     item.addEventListener('change',()=>{
+//       console.log(item)
+//       const img_tag_local = document.querySelector('item img');
+//       console.log(img_tag_local)
+//       if(item.value === 'beef_meet'){
+//
+//         change_img(meat_box,'../img/pirjoala-de-vita.svg')
+//       }
+//       else if(item.value .value === 'breaded_fillet'){
+//         add_img(meat_box,'../img/pirjoala-de-pui.svg')
+//       }else if(item.value .value === 'grilled_fillet'){
+//         add_img(meat_box,'../img/pirjoala-de-pui-grilled.svg')
+//       }
+//     })
 //   })
 // }
-// add_arrow(burger_img_detailes)
+// changes();
+
+const add_img = (box,img_adress) => {
+
+  const div = document.createElement('div') ;
+  div.classList.add('burger_image')
+  const img= document.createElement('img')
+  img.setAttribute('src',img_adress)
+  box.append(div);
+  div.append(img);
+  images = document.querySelectorAll('.burger_image')
+  add_arrow(images)
+}
+const change_img = (img,new_src) => {
+  img.removeAttribute('src');
+  img.setAttribute('src',new_src)
+}
+const add_arrow = (arr) => {
+  arr.forEach((item, index) => {
+    console.log(item);
+    if (index % 2 === 0) {
+      item.classList.add("burger_image_before");
+      item.classList.remove("burger_image_after");
+    } else {
+      item.classList.add("burger_image_after");
+      item.classList.remove("burger_image_before");
+    }
+
+  });
+};
