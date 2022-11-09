9a86b10522c2b8df086d1766791a77a698d86fef
Corina Morari <morarik8@gmail.com>
2022-11-09 16:58:00 +0200

    Done

--- script/script.js
+++ script/script.js
@@ -1,10 +1,12 @@
 const order = [];
 let indexNr = 0;
+//Delete Sign count
 let countMeat = 0;
 let countBSouce = 0;
 let countTSouce = 0;
 let countCheese = 0;
 let countToppings = 0;
+
 let images = document.querySelectorAll(".burger_image");
 // buttons for add selects
 const add_meat_btn = document.querySelector(".btn_meat");
@@ -27,153 +29,165 @@ const toppings_box = document.querySelector(".toppings_box");
 //arrays
 let select_meat_arr = document.querySelectorAll(".burger_item_meat select");
 let select_arr = document.querySelectorAll("select");
-let spans = document.querySelectorAll('.burger_image span')
+let spans = document.querySelectorAll(".burger_image span");
 
 const fetch1 = (category) => {
     const cat = category;
-  fetch("./script/package.json")
-    .then((response) => response.json())
-    .then((data) => {
-        createSelect(data,cat);})
-
+    fetch("./script/package.json")
+        .then((response) => response.json())
+        .then((data) => {
+            createSelect(data, cat);
+        });
 };
 
 add_meat_btn.addEventListener("click", () => {
-    fetch1('meat');
+    fetch1("meat");
 });
-add_bSauce_btn.addEventListener('click',()=>{
-    fetch1('bottom sauce')
-})
-add_tSauce_btn.addEventListener('click',()=>{
-    fetch1('top sauce')
-})
-add_cheese_btn.addEventListener('click',()=>{
-    fetch1('cheese')
-})
-add_toppings_btn.addEventListener('click',()=>{
-    fetch1('toppings')
-})
+add_bSauce_btn.addEventListener("click", () => {
+    fetch1("bottom sauce");
 
+});
+add_tSauce_btn.addEventListener("click", () => {
+    fetch1("top sauce");
+});
+add_cheese_btn.addEventListener("click", () => {
+    fetch1("cheese");
+});
+add_toppings_btn.addEventListener("click", () => {
+    fetch1("toppings");
+});
 
-const createSelect = (data,category) => {
+const createSelect = (data, category) => {
     const cat = data[category];
-    const divParent = document.createElement('div')
-    divParent.classList.add('divParent')
+    indexNr++
+    const divParent = document.createElement("div");
+    divParent.setAttribute('index',indexNr)
+    divParent.classList.add("divParent");
     const select = document.createElement("select");
+    select.className = [category];
     divParent.append(select);
-    select.className=[category]
-    if(category === "meat"){
+
+    if (category === "meat") {
         meat_div.append(divParent);
         countMeat++;
-        if(countMeat > 1) {
+        if (countMeat > 1) {
             deleteSign(divParent);
         }
-    }else if(category === 'bottom sauce'){
-        bottomSauce_div.append(divParent)
+    } else if (category === "bottom sauce") {
+        bottomSauce_div.append(divParent);
         countBSouce++;
-        if(countBSouce > 1) {
+        if (countBSouce > 1) {
             deleteSign(divParent);
         }
-    }else if(category === 'top sauce'){
-        topSauce_div.append(divParent)
+    } else if (category === "top sauce") {
+        topSauce_div.append(divParent);
         countTSouce++;
-        if(countTSouce > 1) {
+        if (countTSouce > 1) {
             deleteSign(divParent);
         }
-    }else if(category === 'cheese'){
+    } else if (category === "cheese") {
         cheese_div.append(divParent);
         countCheese++;
-        if(countCheese > 1) {
+        if (countCheese > 1) {
             deleteSign(divParent);
         }
-    }else if(category === 'toppings'){
-        toppings_div.append(divParent)
+    } else if (category === "toppings") {
+        toppings_div.append(divParent);
         countToppings++;
-        if(countToppings > 1) {
+        if (countToppings > 1) {
             deleteSign(divParent);
         }
     }
-
 
     cat.forEach((element) => {
         const option = document.createElement("option");
-        if(element.price !== undefined){
-        option.textContent = `${element.name.toUpperCase()} (${
-          element.price
-        } lei)`;}else {
-            option.textContent = "-"
+        if (element.price !== undefined) {
+            option.textContent = `${element.name.toUpperCase()} (${
+                element.price
+            } lei)`;
+        } else {
+            option.textContent = "-";
         }
         select.append(option);
+    });
 
-      });
+    if (category === "meat") {
+        const div = document.createElement("div");
+        const img = document.createElement("img");
+        div.setAttribute('index',indexNr)
 
-        if(category === 'meat'){
+        div.classList.add("burger_image");
+        if (category === "meat") {
+            meat_box.append(div);
+        }
 
-      const div = document.createElement('div')
-      const img = document.createElement('img')
+        div.append(img);
+        img.src = cat[0].url;
 
-      div.classList.add('burger_image')
-
-        meat_box.append(div)
-
-
-      div.append(img)
-      img.src = cat[0].url
-
-        const span = document.createElement('span')
+        const span = document.createElement("span");
         span.textContent = cat[0].name;
         div.append(span);
 
-    images = document.querySelectorAll('.burger_image')
-      spans = document.querySelectorAll('.burger_image span')
+        images = document.querySelectorAll(".burger_image");
+        spans = document.querySelectorAll(".burger_image span");
 
-      add_arrow(images,spans);}
-
-}
-
-const add_arrow = (arrArrow,arrSpan) => {
-    arrArrow.forEach((item, index) => {
-    if (index % 2 === 0) {
-      item.classList.add("burger_image_before");
-      item.classList.remove("burger_image_after");
-
-    } else {
-      item.classList.add("burger_image_after");
-      item.classList.remove("burger_image_before");
+        add_arrow(images, spans);
     }
-  });
+};
 
-    arrSpan.forEach((item, index) =>{
-        if(index % 2 === 0){
-            item.classList.add('left_description')
-            item.classList.remove('right_description')
-        }else{
-            item.classList.add('right_description')
-            item.classList.remove('left_description')
+const add_arrow = (arrArrow, arrSpan) => {
+    arrArrow.forEach((item, index) => {
+        if (index % 2 === 0) {
+            item.classList.add("burger_image_before");
+            item.classList.remove("burger_image_after");
+        } else {
+            item.classList.add("burger_image_after");
+            item.classList.remove("burger_image_before");
         }
-    })
+    });
+
+    arrSpan.forEach((item, index) => {
+        if (index % 2 === 0) {
+            item.classList.add("left_description");
+            item.classList.remove("right_description");
+        } else {
+            item.classList.add("right_description");
+            item.classList.remove("left_description");
+        }
+    });
 };
 const deleteSign = (divParent) => {
-    const deleteSelect = document.createElement('div')
-    deleteSelect.setAttribute('class','deleteSelect')
-    divParent.append(deleteSelect)
-    deleteSelect.addEventListener('click',()=>{
-        divParent.remove()
-    })
-}
+    const deleteSelect = document.createElement("div");
+    deleteSelect.setAttribute("class", "deleteSelect");
+    divParent.append(deleteSelect);
+    deleteSelect.addEventListener("click", () => {
+        divParent.remove();
+    });
+};
+window.addEventListener('change',(e)=>{
 
-window.addEventListener('load',()=>{
-    fetch1('meat')
-    fetch1('bottom sauce')
-    fetch1('top sauce')
-    fetch1('cheese')
-    fetch1('toppings')
+    if(e.target.tagName === 'SELECT'){
+        console.log(e.target.value);
+        const index_value = e.target.value.indexOf('(');
+        const span_new = e.target.value.slice(0,index_value);
+        const select_index = e.target.parentElement.getAttribute('index');
+        images = document.querySelectorAll('.burger_image');
+        spans = document.querySelectorAll('span')
+
+        for (let i = 0; i < images.length; i++) {
+            if(images[i].getAttribute('index') !== null && images[i].getAttribute('index') === select_index){
+                console.log(images[i])
+                console.log(spans[i]);
+                spans[i].textContent = span_new
+
+            }
+        }
+    }
 })
-
-
-
-
-
-
-
-
+window.addEventListener("load", () => {
+    fetch1("meat");
+    fetch1("bottom sauce");
+    fetch1("top sauce");
+    fetch1("cheese");
+    fetch1("toppings");
+});
